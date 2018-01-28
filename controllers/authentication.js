351ee40790e8e110448b04444dc7d782a09cfaa6
manikandanj2207 <manikandan.jeeva@ymail.com>
2018-01-28 14:23:43 +0530

    0128108 : 2.23pm

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -15,6 +15,18 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next){
+  /*
+  User has already had their email and password auth'd
+  We just need to give them a token
+  The user detail is attached to the req by the passport LocalStrategy
+  and accesible here
+  */
+
+  res.send({ token: tokenForUser(req.user)}); 
+}
+
+
 exports.signup = function(req, res, next){
   /* res.send({ success: 'true'}); */
   /* console.log(req.body); */
--- router.js
+++ router.js
@@ -1,4 +1,9 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app){
 
@@ -11,5 +16,13 @@ module.exports = function(app){
 
     res.send(['waterbottle','phone','paper']);
     */
+    app.get('/', requireAuth, function(req, res){
+      res.send({ hi: 'there' });
+    });
+    app.post('/signin', requireSignin, Authentication.signin);
     app.post('/signup', Authentication.signup);
   };
+
+
+
+/**/
